6f0cd5d481b19c3cb9acb2a521df0de75de39242
Felix Dumit <felix.dumit@gmail.com>
2017-02-15 21:29:54 -0800

    update copyright

--- PFUser+Digits.m
+++ PFUser+Digits.m
@@ -2,7 +2,7 @@
 //  PFUser+Digits.m
 //
 //  Created by Felix Dumit on 11/6/14.
-//  Copyright (c) 2015 Felix Dumit. All rights reserved.
+//  Copyright (c) 2017 Felix Dumit. All rights reserved.
 //
 
 #import <Bolts/Bolts.h>
